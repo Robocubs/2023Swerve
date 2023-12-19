cbe5ac53c4782c332e08a4fd7ca8a13cc6e1cc0a
Daniel Burke <danielburke1206@gmail.com>
2023-12-19 17:38:19 -0500

    upgraded libraries to 2024 versions

---
+++ src/main/java/com/team1701/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.team1701.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "2023Swerve-Imported";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 4;
+    public static final String GIT_SHA = "0469465dd0428b73b366abea5ba73c4df849cad6";
+    public static final String GIT_DATE = "2023-09-11 17:09:58 EDT";
+    public static final String GIT_BRANCH = "physical-robot";
+    public static final String BUILD_DATE = "2023-12-04 19:50:40 EST";
+    public static final long BUILD_UNIX_TIME = 1701737440228L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
